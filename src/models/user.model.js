8bfa6151981120a7c7a46f3109ee8c7cefce6403
Monish0210 <monishpatel831@gmail.com>
2025-06-09 09:04:17 +0530

    fix bug in auth.middleware and login logout controllers and added refreshAccessToken endpoint

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -57,10 +57,10 @@ userSchema.methods.isPasswordCorrect = async function (password){
     return await bcrypt.compare(password, this.password)
 }
 
-userSchema.methods.generateAccessToken = async function (){
+userSchema.methods.generateAccessToken = function (){
     return jwt.sign(
         {
-            _id: this.id,
+            _id: this._id,
             email: this.email,
             username: this.username,
             fullName: this.fullName
@@ -71,10 +71,10 @@ userSchema.methods.generateAccessToken = async function (){
         }
     )
 }
-userSchema.methods.generateRefreshToken = async function (){
+userSchema.methods.generateRefreshToken = function (){
     return jwt.sign(
         {
-            _id: this.id,
+            _id: this._id,
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
