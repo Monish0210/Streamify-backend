c85f235fcfa918ab5c9a290141a3c5743b519389
Monish0210 <monishpatel831@gmail.com>
2025-12-29 22:32:12 +0530

    fix: update response status code for successful user registration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -93,7 +93,7 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     return res.status(201).json(
-        new ApiResponse(200, createdUser, "User Registered Successfully")
+        new ApiResponse(201, createdUser, "User Registered Successfully")
     )
 })
 
